218b2e458d2a544c8b3096c94a256ee49ea0b5de
Tyler Hobbs <tyler@datastax.com>
2012-09-27 22:40:43 -0500

    Debian packaging

--- setup.py
+++ setup.py
@@ -1,92 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
-
-try:
-    import subprocess
-    has_subprocess = True
-except:
-    has_subprocess = False
-
-try:
-    from ez_setup import use_setuptools
-    use_setuptools()
-except ImportError:
-    pass
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-from distutils.cmd import Command
+from distutils.core import setup
 
 import puresasl
 
-class doc(Command):
-
-    description = "generate or test documentation"
-
-    user_options = [("test", "t",
-                     "run doctests instead of generating documentation")]
-
-    boolean_options = ["test"]
-
-    def initialize_options(self):
-        self.test = False
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        if self.test:
-            path = "doc/_build/doctest"
-            mode = "doctest"
-        else:
-            path = "doc/_build/%s" % puresasl.__version__
-            mode = "html"
-
-            try:
-                os.makedirs(path)
-            except:
-                pass
-
-        if has_subprocess:
-            status = subprocess.call(["sphinx-build", "-b", mode, "doc", path])
-
-            if status:
-                raise RuntimeError("documentation step '%s' failed" % mode)
-
-            print ""
-            print "Documentation step '%s' performed, results here:" % mode
-            print "   %s/" % path
-        else:
-            print """
-`setup.py doc` is not supported for this version of Python.
-
-Please ask in the user forums for help.
-"""
-
-
-setup(
-    name='pure-sasl',
-    version=puresasl.__version__,
-    author='Tyler Hobbs',
-    author_email='tylerlhobbs@gmail.com',
-    description='Pure Python client SASL implementation',
-    url='http://github.com/thobbs/pure-sasl',
-    keywords='sasl',
-    packages=['puresasl'],
-    cmdclass={"doc": doc},
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ]
-)
+setup(name='pure-sasl',
+      version=puresasl.__version__,
+      author='Tyler Hobbs',
+      author_email='tylerlhobbs@gmail.com',
+      description='Pure Python client SASL implementation',
+      url='http://github.com/thobbs/pure-sasl',
+      keywords='sasl',
+      packages=['puresasl'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ]
+    )
