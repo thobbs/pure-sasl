a613fcf7fb9b51c1398f857097a81c5d516f02fc
Tyler Hobbs <tyler@datastax.com>
2013-09-17 11:23:44 -0500

    Bump version to 0.1.5, update changelog

---
+++ CHANGES.rst
@@ -0,0 +1,3 @@
+0.1.5
+=====
+* Fix digest URI order for DigestMD5Mechanism
--- puresasl/__init__.py
+++ puresasl/__init__.py
@@ -1,5 +1,5 @@
-__version__ = '0.1.4'
-__version_info__ = (0, 1, 4)
+__version__ = '0.1.5'
+__version_info__ = (0, 1, 5)
 
 
 class SASLError(Exception):
